7d5b56656a7954591582039e1ea3137933cc42c5
Alki <Alki@Alkis.com>
2025-08-30 13:09:15 +0300

    Valgrind first testing

--- point/main.cpp
+++ point/main.cpp
@@ -5,17 +5,17 @@
 using namespace std;
 
 
+uint8_t * x = new uint8_t[100];
 
 int just_to_exit_later(int x , int y)
 {
     point * p1 = new point();
-    p1->setX(99);
+
+    /*p1->setX(99);
     p1->setY(49);
-    printf("El friend function deh %d00%% \n", testing_point(*p1, 99, 49));
-
-    testing_point_class testing_point_object(*p1);
-
-    printf("El friend class deh %d00%% \n", testing_point_object.testing_point_class_function(99,49));
+    printf("El friend function deh %d00%% \n", testing_point(*p1, 99, 49));*/
+    //testing_point_class testing_point_object(*p1);
+    //printf("El friend class deh %d00%% \n", testing_point_object.testing_point_class_function(99,49));
     //printf("El friend class using reference deh %d00%% \n", testing_point_object.testing_point_class_function_using_reference(99,49));
 
     point * p2 = new point(*p1); 
@@ -30,7 +30,7 @@ int just_to_exit_later(int x , int y)
     delete p1;
     delete p2;
     delete p3;
-    delete p4;
+    //delete p4;
     
     return 0;
 }
@@ -56,6 +56,22 @@ int just_to_exit_later(int z)
 
 int main() {
 
+    struct x
+    {
+        uint8_t y;
+        uint8_t *z;
+    };
+    x *b = new x[100];
+    b[0].z = new uint8_t[100];
+    delete[] b;
+
+    struct Node
+    {
+        int *data;
+    };
+    Node *n = new Node;
+    n->data = new int[10];
+    //delete n; // but forgot delete[] n->data;
 
     //just_to_exit_later(5,4.0);
     //just_to_exit_later(5,4.0f);    
